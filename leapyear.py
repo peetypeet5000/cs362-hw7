f7aa769ff6925a42e4edc8419aabc71db536de2d
Peter LaMontagne <49214098+peetypeet5000@users.noreply.github.com>
2021-05-31 22:14:11 -0700

    implimented 1st test, years mod 4 are leap

--- leapyear.py
+++ leapyear.py
@@ -1,4 +1,8 @@
 #takes an integer year i and returns a string indicating if the year is a leap year or not
-def leapyear(i):
-    pass
+def leapyear(year):
+    output = ""
+    if year % 4 == 0:
+        output = str(year) + " is a leap year"
+
+    return output
     
