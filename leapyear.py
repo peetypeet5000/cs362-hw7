3883d1b8605d31a188527844a9181135a19dfa51
Peter LaMontagne <49214098+peetypeet5000@users.noreply.github.com>
2021-05-31 22:21:18 -0700

    implimented 4th test, years devis by 400 are leap

--- leapyear.py
+++ leapyear.py
@@ -3,7 +3,10 @@ def leapyear(year):
     output = ""
     if year % 4 == 0:
         if year % 100 == 0:
-            output = str(year) + " is not a leap year"
+            if year % 400 == 0:
+                output = str(year) + " is a leap year"
+            else:
+                output = str(year) + " is not a leap year"
         else:
             output = str(year) + " is a leap year"
     else:
