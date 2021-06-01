94a695183e13e29d2329592331eb372744d24489
Peter LaMontagne <49214098+peetypeet5000@users.noreply.github.com>
2021-05-31 21:43:37 -0700

    wrote second fizzbuzz test, testing if mult. of 5 get Buzz

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -5,3 +5,5 @@ class TestFizzbuzz:
     def test(self):
         for i in [3, 6, 9, 12]:
             assert fizzbuzz.fizzbuzz(i) == "Fizz"
+        for i in [5, 10, 15, 20]:
+            assert fizzbuzz.fizzbuzz(i) == "Buzz"
