b6e8c6028ad7f223510ced38ba311985e6506067
Peter LaMontagne <49214098+peetypeet5000@users.noreply.github.com>
2021-05-31 22:15:52 -0700

    wrote second test, if not devis by 4, return that it is not a leap year

--- test_leapyear.py
+++ test_leapyear.py
@@ -3,5 +3,10 @@ import leapyear
 
 class TestLeapyear:
     def test(self):
+        #first test
         assert leapyear.leapyear(4) == "4 is a leap year"
         assert leapyear.leapyear(8) == "8 is a leap year"
+
+        #second test
+        assert leapyear.leapyear(3) == "3 is not a leap year"
+        assert leapyear.leapyear(2001) == "2001 is not a leap year"
