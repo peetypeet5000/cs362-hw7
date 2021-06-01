1561c407b448768630c15b7892ed8f762e1697fc
Peter LaMontagne <49214098+peetypeet5000@users.noreply.github.com>
2021-05-31 21:52:06 -0700

    added fourth test which checks it returns the number when no condition is true

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -7,6 +7,7 @@ class TestFizzbuzz:
             assert fizzbuzz.fizzbuzz(i) == "Fizz"
         for i in [5, 10, 20]:
             assert fizzbuzz.fizzbuzz(i) == "Buzz"
-
         for i in [15, 30, 60]:
             assert fizzbuzz.fizzbuzz(i) == "FizzBuzz"
+        for i in [1, 2, 4, 7, 61, 88]:
+            assert fizzbuzz.fizzbuzz(i) == i
